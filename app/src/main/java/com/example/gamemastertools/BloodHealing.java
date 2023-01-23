89d3542d420a96e924d4d8aa4a75dd694276fbf6
PG <aakilae@gmail.com>
2023-01-23 16:30:57 +0100

    healing table as table

--- app/src/main/java/com/example/gamemastertools/BloodHealing.java
+++ app/src/main/java/com/example/gamemastertools/BloodHealing.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,5 +28,13 @@ public class BloodHealing extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.btbBloodHealingTablePrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(BloodHealing.this)
+                        .navigate(R.id.action_blood_healing_table_to_blood);
+            }
+        });
     }
 }
