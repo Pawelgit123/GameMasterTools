c8b7e0ddb79010cfd56027cfecaad14ffd98cb74
PG <aakilae@gmail.com>
2023-02-01 17:52:01 +0100

    Panic Roll

--- app/src/main/java/com/example/gamemastertools/AlienPanicRoll.java
+++ app/src/main/java/com/example/gamemastertools/AlienPanicRoll.java
@@ -1,23 +1,81 @@
 package com.example.gamemastertools;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.gamemastertools.alien.AlienPanicRoller;
+import com.example.gamemastertools.databinding.FragmentAlienPanicRollBinding;
+
 public class AlienPanicRoll extends Fragment {
 
+    private FragmentAlienPanicRollBinding binding;
+    private int panicLevel = 1;
+    private int panicLevelRolled = 0;
+    private String panicTitle = "";
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentAlienPanicRollBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_alien_panic_roll, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.txtCurrentPanicLevel.setText(String.valueOf(panicLevel));
+        binding.textPanicRollNumberRolled.setText(String.valueOf(panicLevelRolled));
+        binding.textViewPanicTitle.setVisibility(View.INVISIBLE);
+        binding.textPanicRollDescription.setVisibility(View.INVISIBLE);
+
+        binding.btnPlusPanicLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(panicLevel<15){
+                    panicLevel++;
+                }
+                binding.txtCurrentPanicLevel.setText(String.valueOf(panicLevel));
+            }
+        });
+        binding.btnMinusPanicLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(panicLevel > 1){
+                    panicLevel--;
+                }
+                binding.txtCurrentPanicLevel.setText(String.valueOf(panicLevel));
+            }
+        });
+
+        binding.btnPanicRoll.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+
+
+                panicLevelRolled = AlienPanicRoller.panicRoll(panicLevel);
+                binding.textPanicRollNumberRolled.setText(String.valueOf(panicLevelRolled));
+                panicTitle = AlienPanicRoller.panicTitle(panicLevelRolled);
+
+                binding.textViewPanicTitle.setText(panicTitle);
+                binding.textViewPanicTitle.setVisibility(View.VISIBLE);
+                binding.textPanicRollDescription.setText(AlienPanicRoller.panicDescription(panicTitle));
+                binding.textPanicRollDescription.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+
     }
 }
